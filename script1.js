4447aab0333556946e5b02915357e4379b82fdff
Krumpich <krumpich@gmail.com>
2022-10-15 15:32:56 +0200

    added few lines of email sending script

--- script1.js
+++ script1.js
@@ -3,9 +3,30 @@ function fun() {
 }
 
 
-const Button = document.body.className("Button")
 
-Button.addEventListener("mouseover" , e=> {
 
-    console.log("entered");
-})
+function SendMail() {
+
+
+    var params = {
+
+        from_name: "Customer",
+        message: "Hello, bla bla bla... Do this:",
+
+                file: [
+
+            {filename: "Test_Cube.stl", path: "./Stl_Files/Test_Cube.stl"}
+
+        ]
+
+/*        file:  document.getElementById("Stl_File")    */
+
+    }
+
+    emailjs.send("service_d91d47k","template_f2mer6e",params).then(function (res){
+       
+        alert("Success! " + res.status);
+
+        })
+
+}
